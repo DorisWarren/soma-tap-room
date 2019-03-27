2175ca825c8b52133d2ace70e7196e571752f9f2
Student <example@example.com>
2019-03-27 14:31:53 -0700

    create soma tap room template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SomaComponent } from './soma/soma.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SomaComponent
   ],
   imports: [
     BrowserModule
